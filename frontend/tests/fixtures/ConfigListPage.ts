3a68c96175d72976c1e60a12709ed9cc748584c0
Sebastian M <sebastian@mobiflight.com>
2025-11-18 18:58:02 +0000

    add missing properties for test project

--- frontend/tests/fixtures/ConfigListPage.ts
+++ frontend/tests/fixtures/ConfigListPage.ts
@@ -35,6 +35,8 @@ export class ConfigListPage {
         Name: "Test Project",
         FilePath: "SomeFilePath.mfproj",
         ConfigFiles: [],
+        Sim: "msfs",
+        UseFsuipc: false,
       } as Project,
     }
     await this.mobiFlightPage.publishMessage(message)
