5996fb3a0716c8a5a106724d58fc1c5b9804b2d1
Sebastian M <sebastian@mobiflight.com>
2025-12-06 22:45:59 +0100

    Add custom converter

---
+++ Base/Serialization/Json/PreconditionConverter.cs
@@ -0,0 +1,78 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+
+namespace MobiFlight.Base.Serialization.Json
+{
+    public class PreconditionConverter : JsonConverter
+    {
+        public override bool CanRead => false;
+        public override bool CanWrite => true;
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            var p = value as Precondition;
+            if (IsNullOrEmpty(p))
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            // Create a resolver that ignores the JsonConverter attribute on Precondition
+            var resolver = new IgnoreAttributeContractResolver();
+            var tmp = new JsonSerializer
+            {
+                ContractResolver = resolver,
+                Culture = serializer.Culture,
+                Formatting = serializer.Formatting,
+                NullValueHandling = serializer.NullValueHandling,
+                DefaultValueHandling = serializer.DefaultValueHandling,
+                TypeNameHandling = serializer.TypeNameHandling
+            };
+
+            foreach (var c in serializer.Converters)
+            {
+                if (c.GetType() != this.GetType())
+                    tmp.Converters.Add(c);
+            }
+
+            tmp.Serialize(writer, value);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            throw new NotSupportedException("ReadJson is disabled via CanRead = false.");
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(Precondition).IsAssignableFrom(objectType);
+        }
+
+        public static bool IsNullOrEmpty(Precondition p)
+        {
+            return p == null || (
+                p.Type == "none" &&
+                p.Ref == null &&
+                p.Serial == null &&
+                p.Pin == null &&
+                p.Value == null);
+        }
+
+        private class IgnoreAttributeContractResolver : DefaultContractResolver
+        {
+            protected override JsonContract CreateContract(Type objectType)
+            {
+                var contract = base.CreateContract(objectType);
+                
+                // Remove converter from Precondition to avoid recursion
+                if (objectType == typeof(Precondition))
+                {
+                    contract.Converter = null;
+                }
+                
+                return contract;
+            }
+        }
+    }
+}
