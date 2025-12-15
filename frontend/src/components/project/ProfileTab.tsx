76edf2c4efdbe38e21131c182d9594369507b906
Sebastian M <sebastian@mobiflight.com>
2025-12-15 12:58:11 +0000

    Fix text color on edit and hovering outside input field

--- frontend/src/components/project/ProfileTab.tsx
+++ frontend/src/components/project/ProfileTab.tsx
@@ -8,7 +8,6 @@ import { forwardRef, useCallback, useEffect, useRef, useState } from "react"
 import { cn } from "@/lib/utils"
 import { InlineEditLabel, InlineEditLabelRef } from "../InlineEditLabel"
 import { useDroppable } from "@dnd-kit/core"
-import { clamp } from "lodash-es"
 import ProfileTabContextMenu from "@/components/project/ProfileTab/ProfileTabContextMenu"
 
 export interface ProfileTabProps extends VariantProps<typeof buttonVariants> {
@@ -19,133 +18,131 @@ export interface ProfileTabProps extends VariantProps<typeof buttonVariants> {
   resizeCallback?: () => void
 }
 
-export const ProfileTab = forwardRef<HTMLDivElement, ProfileTabProps>(({
-  file,
-  index,
-  variant,
-  totalCount,
-  selectActiveFile: onSelectActiveFile,
-  resizeCallback
-}: ProfileTabProps, ref) => {
-  const { publish } = publishOnMessageExchange()
-  const inlineEditRef = useRef<InlineEditLabelRef>(null)
-  const buttonRef = useRef<HTMLButtonElement>(null)
+export const ProfileTab = forwardRef<HTMLDivElement, ProfileTabProps>(
+  (
+    {
+      file,
+      index,
+      variant,
+      totalCount,
+      selectActiveFile: onSelectActiveFile,
+      resizeCallback,
+    }: ProfileTabProps,
+    ref,
+  ) => {
+    const { publish } = publishOnMessageExchange()
+    const inlineEditRef = useRef<InlineEditLabelRef>(null)
+    const buttonRef = useRef<HTMLButtonElement>(null)
 
-  const label = file.Label ?? file.FileName
-  const [ optimisticLabel, setOptimisticLabel ] = useState(label)
+    const label = file.Label ?? file.FileName
+    const [optimisticLabel, setOptimisticLabel] = useState(label)
 
-  const isActiveTab = variant === "tabActive"
+    const isActiveTab = variant === "tabActive"
 
-  useEffect(() => {
-    setOptimisticLabel(label)
-  }, [label])
+    useEffect(() => {
+      setOptimisticLabel(label)
+    }, [label])
 
-  const onSave = (newLabel: string) => {
-    setOptimisticLabel(newLabel)
-    publish({
-      key: "CommandFileContextMenu",
-      payload: {
-        action: "rename",
-        index: index,
-        file: {
-          ...file,
-          Label: newLabel,
+    const onSave = (newLabel: string) => {
+      setOptimisticLabel(newLabel)
+      publish({
+        key: "CommandFileContextMenu",
+        payload: {
+          action: "rename",
+          index: index,
+          file: {
+            ...file,
+            Label: newLabel,
+          },
         },
+      } as CommandFileContextMenu)
+    }
+
+    const groupHoverStyle =
+      variant === "tabActive"
+        ? "group-hover:bg-primary group-hover:text-primary-foreground"
+        : "group-hover:bg-accent group-hover:text-accent-foreground"
+
+    // this ensures correct text color
+    // when we are in edit mode
+    // but hover outside of the input field, e.g. the menu button
+    const groupHoverInputStyle = "group-hover:text-foreground"
+
+    const { setNodeRef } = useDroppable({
+      id: `file-button-${index}`,
+      data: {
+        type: `tab`,
+        index: index,
       },
-    } as CommandFileContextMenu)
-  }
-  
-  const groupHoverStyle =
-    variant === "tabActive"
-      ? "group-hover:bg-primary group-hover:text-primary-foreground"
-      : "group-hover:bg-accent group-hover:text-accent-foreground"
-
-  const { setNodeRef } = useDroppable({
-    id: `file-button-${index}`,
-    data: {
-      type: `tab`,
-      index: index,
-    },
-  })
-
-  const labelWidthBasedOnCount: Record<number, string> = {
-    0: "max-w-60 xl:max-w-80 3xl:max-w-65",
-    // 1: "max-w-50 xl:max-w-70 3xl:max-w-65",
-    // 2: "max-w-40 xl:max-w-60 3xl:max-w-65",
-    // 3: "max-w-30 xl:max-w-50 3xl:max-w-65",
-    // 4: "max-w-20 xl:max-w-40 3xl:max-w-65",
-    // 5: "max-w-10 xl:max-w-30 3xl:max-w-65",
-    // 6: "max-w-5 xl:max-w-20 3xl:max-w-50 4xl:max-w-60",
-    // 7: "max-w-3 xl:max-w-7 3xl:max-w-30 4xl:max-w-50",
-    // 8: "max-w-2 xl:max-w-7 3xl:max-w-25 4xl:max-w-40",
-    // 9: "max-w-2 xl:max-w-7 3xl:max-w-20 4xl:max-w-35",
-    // 10: "max-w-2 xl:max-w-15 3xl:max-w-15 4xl:max-w-30",
-    // 11: "max-w-2 xl:max-w-5 3xl:max-w-10 4xl:max-w-25",
-    // 12: "max-w-2 xl:max-w-5 3xl:max-w-5 4xl:max-w-20",
-  }
-
-  const maxInputWidth = labelWidthBasedOnCount[clamp(totalCount, 0, 0)]
-  const labelClassName = `truncate transition-all duration-300 ease-in-out ${labelWidthBasedOnCount[clamp(totalCount, 0, 0)]}`
-
-  const combinedRef = useCallback((node: HTMLDivElement | null) => {
-    setNodeRef(node)
-    if (typeof ref === 'function') ref(node)
-    else if (ref) ref.current = node
-  }, [ref, setNodeRef])
-
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver(() => {
-      resizeCallback?.()
     })
 
-    const button = buttonRef.current
-    if (button) {
-      resizeObserver.observe(button)
-    }
+    const maxInputWidth = "max-w-60 xl:max-w-70 3xl:max-w-80"
+    const labelClassName = `truncate transition-all duration-300 ease-in-out ${maxInputWidth}`
 
-    return () => {
+    const combinedRef = useCallback(
+      (node: HTMLDivElement | null) => {
+        setNodeRef(node)
+        if (typeof ref === "function") ref(node)
+        else if (ref) ref.current = node
+      },
+      [ref, setNodeRef],
+    )
+
+    useEffect(() => {
+      const resizeObserver = new ResizeObserver(() => {
+        resizeCallback?.()
+      })
+
+      const button = buttonRef.current
       if (button) {
-        resizeObserver.unobserve(button)
+        resizeObserver.observe(button)
       }
-    }
-  }, [resizeCallback])
 
-  return (
-    <div
-      className={`group flex flex-row justify-center border-b`}
-      ref={combinedRef}
-      role="tab"
-      aria-selected={isActiveTab}
-      title={optimisticLabel}
-    >
-      <Button      
-        ref={buttonRef}
-        variant={variant}
-        value={optimisticLabel}
-        className={cn(
-          groupHoverStyle,
-          "rounded-r-none rounded-b-none border-r-0",
-          maxInputWidth
-        )}
-        onClick={() => onSelectActiveFile(index)}
+      return () => {
+        if (button) {
+          resizeObserver.unobserve(button)
+        }
+      }
+    }, [resizeCallback])
+
+    return (
+      <div
+        className={`group flex flex-row justify-center border-b`}
+        ref={combinedRef}
+        role="tab"
+        aria-selected={isActiveTab}
+        title={optimisticLabel}
       >
-        <InlineEditLabel
-          labelClassName={labelClassName}
-          ref={inlineEditRef}
+        <Button
+          ref={buttonRef}
+          variant={variant}
           value={optimisticLabel}
-          onSave={onSave}
-          disabled={!isActiveTab}
+          className={cn(
+            groupHoverStyle,
+            "rounded-r-none rounded-b-none border-r-0",
+            maxInputWidth,
+          )}
+          onClick={() => onSelectActiveFile(index)}
+        >
+          <InlineEditLabel
+            labelClassName={labelClassName}
+            inputClassName={groupHoverInputStyle}
+            ref={inlineEditRef}
+            value={optimisticLabel}
+            onSave={onSave}
+            disabled={!isActiveTab}
+          />
+        </Button>
+        <ProfileTabContextMenu
+          variant={variant}
+          groupHoverStyle={groupHoverStyle}
+          index={index}
+          file={file}
+          inlineEditRef={inlineEditRef}
         />
-      </Button>
-      <ProfileTabContextMenu 
-        variant={variant}
-        groupHoverStyle={groupHoverStyle}
-        index={index}
-        file={file}
-        inlineEditRef={inlineEditRef}
-      />
-    </div>
-  )
-})
+      </div>
+    )
+  },
+)
 
 ProfileTab.displayName = "ProfileTab"
