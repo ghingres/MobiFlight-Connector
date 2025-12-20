0bc3b1873e5f8c4a23f6bafd15fdb892f8fc8f7d
Sebastian M <sebastian@mobiflight.com>
2025-12-20 15:04:19 +0100

    add comment

--- MobiFlight/Joysticks/Joystick.cs
+++ MobiFlight/Joysticks/Joystick.cs
@@ -36,6 +36,9 @@ namespace MobiFlight
         protected JoystickState State = null;
         private HidStream Stream;
 
+        /// <summary>
+        /// This map defines how a usageId maps to a JoystickState property name.
+        /// </summary>
         private static readonly Dictionary<int, string> UsageMap = new Dictionary<int, string>
         {
             [48] = "X",
