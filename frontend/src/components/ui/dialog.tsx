2a3318294c4d31c77f8dea65995030a0cf470d21
Sebastian M <sebastian@mobiflight.com>
2025-12-26 09:34:15 +0000

    Remove dependency to lucide icons

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -2,9 +2,9 @@
 
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
-import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
+import { IconX as X } from "@tabler/icons-react"
 
 const Dialog = DialogPrimitive.Root
 
